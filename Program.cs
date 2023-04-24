2c1a76afaa77df82ac1ee48fb77f7a5747c4f419
Sword <swordacegaming@gmail.com>
2023-04-24 10:28:42 +0700

    2

--- Program.cs
+++ Program.cs
@@ -6,29 +6,28 @@
         Type[i] = Console.ReadLine();
     }
     Console.WriteLine(Type[1]);
+    
+    string[] Type = new string[10];
+    for (int i = 0;i < 10; i++) {
+        Type[i] = Console.ReadLine();
+    }
+    Console.WriteLine(Type[1]);
      }
 
+
   static string UserInputType(string[] Type) {
-        string input;
-
-        while(true) {
-        Console.Write("Input Item Type : ");
-        input = Console.ReadLine();
-        } 
-        return input;
+        string[] Type = new string[10];
+    for (int i = 0;i < 10; i++) {
+        Type[i] = Console.ReadLine();
+    }
+        return Type;
     }
 
-    static string UserInputItem(string[] BagAll) {
-        string input;
-
-        while(true) {
-        Console.Write("Input Item Name : ");
-        input = Console.ReadLine();
-        } 
-        return input;
+    static string UserInputType(string[] Name) {
+        string[] Name = new string[10];
+    for (int i = 0;i < 10; i++) {
+        Name[i] = Console.ReadLine();
     }
-static void PrintBoard(char[] Type) {
-        Console.WriteLine("  {0}  |  {1}  |  {2}  ", Type[0],Type[1],Type[2]);
-        Console.WriteLine("_____|_____|_____");
-}
+        return Name;
+    }
 }
