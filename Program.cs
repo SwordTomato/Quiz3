292575317713ba9cc93884567f7ad18519950a15
Sword <swordacegaming@gmail.com>
2023-04-24 10:51:31 +0700

    4

--- Program.cs
+++ Program.cs
@@ -1,31 +1,36 @@
 ﻿class program {
   static void Main(string[] arg) {
-        string[] A = UserInputType(Type);
-        string[] B = UserInputName(Name);
+        string[] A = UserInputType(Typee[]);
+        string[] B = UserInputName(Namee[]);
         
         string search = Console.ReadLine();
         
-        if(search == "Show All")
+        if(search == "Show All") {
             for(int i = 0;i < 10;i++) {
-                Console.WriteLine(name[i]);
+                Console.WriteLine("{0}",A[i]);
+            } if(search = B[i]) {
+                Console.WriteLine("{0}",A[i]);
+            } else {
+                Console.WriteLine("End");
             }
      }
 
 
-  static string UserInputType(string[] Type) {
-        string[] Type = new string[10];
+  static string UserInputType(string[] Typee) {
+        string[] input = new string[10];
     for (int i = 0;i < 10; i++) {
-        Type[i] = Console.ReadLine();
+        input[i] = Console.ReadLine();
     }
-        return Type[10];
+        return input[10];
     }
 
-    static string UserInputName(string[] Name) {
-        string[] Name = new string[10];
+    static string UserInputName(string[] Namee) {
+        string[] input = new string[10];
     for (int i = 0;i < 10; i++) {
-        Name[i] = Console.ReadLine();
+        input[i] = Console.ReadLine();
     }
-        return Name[10];
+        return input[10];
     }
+}
 }
     
